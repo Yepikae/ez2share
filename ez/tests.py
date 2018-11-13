372648a22d1e10eb2eb5d55db3b5dc6e9359742e
yepikae <remi.casado@protonmail.com>
2018-11-13 15:08:23 +0100

    Repository model creation.

--- ez/tests.py
+++ ez/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Repository
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class RepositoryModelTests(TestCase):
+
+    def test_create_repository(self):
+        """
+        Check the creation of a repository.
+        """
+        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        user.save()
+        repository = Repository(owner=user)
+        repository.save()
+        self.assertNotEqual(repository.id, 0)
