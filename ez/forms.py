eed2f9cf914045cd2caacea3654914629f5cc241
Friedrich Hemer <friedrich.hemer@gmail.com>
2018-11-15 13:30:54 +0100

    add ask for repo behaviour

--- ez/forms.py
+++ ez/forms.py
@@ -3,6 +3,8 @@ from django import forms
 class PasswordForm(forms.Form):
     repository_password = forms.CharField(label='Password', max_length=100)
 
-
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+class RepoForm(forms.Form):
+    repository_name = forms.CharField(label='Repository Name', max_length=100)
